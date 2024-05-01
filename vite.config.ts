d109b92eca56c3b12e56fdd344f370e5e3ab5f55
Ilya <wrqqqrrr@gmail.com>
2024-05-01 21:06:36 +0300

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/wrqqqr/test-omg/',
+  base: 'https://wrqqqr.github.io',
   plugins: [
     vue(),
     VueDevTools(),
